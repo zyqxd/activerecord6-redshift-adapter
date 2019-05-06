693cec685e4f5f4392c52f6306b20702d11b90ea
Edward <duan.baochen@gmail.com>
2019-05-06 18:39:31 +0800

    Update column.rb

--- lib/active_record/connection_adapters/redshift/column.rb
+++ lib/active_record/connection_adapters/redshift/column.rb
@@ -3,8 +3,8 @@ module ActiveRecord
     class RedshiftColumn < Column #:nodoc:
       delegate :oid, :fmod, to: :sql_type_metadata
 
-      def initialize(name, default, sql_type_metadata, null = true, table_name= nil, default_function = nil)
-        super name, default, sql_type_metadata, null, table_name, default_function, nil
+      def initialize(name, default, sql_type_metadata, null = true, default_function = nil)
+        super name, default, sql_type_metadata, null, default_function
       end
     end
   end
