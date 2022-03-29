04c50b6b936ff6a797621f38035dc2bfcaa2a276
David Zhang <zyq.david@gmail.com>
2022-03-29 13:11:51 -0400

    Delegate array

--- lib/active_record/connection_adapters/redshift/column.rb
+++ lib/active_record/connection_adapters/redshift/column.rb
@@ -1,7 +1,7 @@
 module ActiveRecord
   module ConnectionAdapters
     class RedshiftColumn < Column #:nodoc:
-      delegate :oid, :fmod, to: :sql_type_metadata
+      delegate :oid, :fmod, :array?, to: :sql_type_metadata
 
       def initialize(name, default, sql_type_metadata, null = true, default_function = nil)
         super name, default, sql_type_metadata, null, default_function
